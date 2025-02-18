7778a30d7365405280c727d58a498e5f622cbe66
levans7 <120535070+levans7@users.noreply.github.com>
2025-02-18 02:05:33 -0500

    update dependencies and now the frontend is serving files from the dist folder

--- frontend/server.js
+++ frontend/server.js
@@ -10,11 +10,16 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 // Serve static files from the root directory
-app.use(express.static(__dirname));
+//app.use(express.static(__dirname));
+app.use(express.static(path.join(__dirname, 'dist')));
+
 
 // Handle all routes by serving the index.html file
-app.get('/login', (req, res) => {
+/*app.get('/login', (req, res) => {
   res.sendFile(path.join(__dirname, 'index.html'));
+});*/
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'dist', 'index.html'));
 });
 
 app.listen(port, () => {
